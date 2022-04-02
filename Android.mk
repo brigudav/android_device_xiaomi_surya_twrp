61fdf7b266425a616cdad7d5aee0b0ac3dd4c989
Alex <alexvl1972@hotmail.com>
2022-04-02 13:23:43 +1000

    surya: add hack some props

--- Android.mk
+++ Android.mk
@@ -8,4 +8,10 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),surya)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+# Hack some props to allow stock ROM flashing
+BOARD_RECOVERY_IMAGE_PREPARE := \
+    sed -i 's/ro.product.board=.*/ro.product.board=surya/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
+    sed -i 's/ro.bootimage.build.date.utc=.*/ro.bootimage.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
+    sed -i 's/ro.build.date.utc=.*/ro.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default;
 endif
