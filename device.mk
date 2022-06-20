9dd6eb4798b6863ddd8194dfb4405ec2e8eb2ac0
Alex <alexvl1972@hotmail.com>
2022-06-20 21:07:50 +1000

    Update device.mk

--- device.mk
+++ device.mk
@@ -23,13 +23,6 @@ PRODUCT_SOONG_NAMESPACES += \
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Apex libraries
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libandroidicu
-
-PRODUCT_COPY_FILES += \
-    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
-
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
