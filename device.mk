f5c32108676cf385b474876584c492858c42f277
brigudav <brigudav@gmail.com>
2022-06-25 11:15:36 +1000

    fix a12

--- device.mk
+++ device.mk
@@ -7,18 +7,25 @@
 # Define hardware platform
 PRODUCT_PLATFORM := sm6150
 
+# Boot control HAL 1.2
+#PRODUCT_PACKAGES += \
+#    android.hardware.boot@1.2-service
+
+#PRODUCT_COPY_FILES += \
+#    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/system/lib64/android.hardware.boot@1.2.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/android.hardware.boot@1.2.so
+
 # Fastbootd
 PRODUCT_PACKAGES += \
-	android.hardware.fastboot@1.0-impl-mock \
-	android.hardware.fastboot@1.0-impl-mock.recovery \
-	fastbootd
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
 
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-	$(LOCAL_PATH)
+    $(LOCAL_PATH)
 
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -26,11 +33,13 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
     libion \
-    libxml2
+    libxml2 \
+    android.hardware.boot@1.2-service
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/android.hardware.boot@1.2.so
 
 # Overrides
 PRODUCT_BUILD_PROP_OVERRIDES += \
