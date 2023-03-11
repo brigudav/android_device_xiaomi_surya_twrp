6e19b9d285aa20bcc77228bf2acd33f6878049c6
Alexander <alexvl1972@hotmail.com>
2023-03-11 13:46:37 +1000

    patch was created in the wrong place)

---
+++ recovery/root/system/bin/patchfstab.sh
@@ -0,0 +1,51 @@
+#!/system/bin/sh
+#
+# This script is needed to support wrappedkey and fbe v2
+# Version: 1.3
+# Date : 2023/03/11
+# Author : brigudav
+#
+
+reclog=/tmp/recovery.log
+tmpv=/tmp/vendor_fstab
+fstabd=$tmpv/etc/fstab.default
+fstabq=$tmpv/etc/fstab.qcom
+fstabr=/system/etc/recovery.fstab
+
+send_log() {
+	echo "I:$1" >> $reclog
+}
+
+finish() {
+	sleep 1
+	umount $tmpv || true
+	rmdir $tmpv
+	exit 1
+}
+
+patch_fstab() {
+	mkdir -p $tmpv
+	mount -o ro -t auto /dev/block/mapper/vendor $tmpv || true
+
+	for i in $fstabq $fstabd; do
+		if [ -f $i ]; then
+		send_log "Find $i"
+			if [[ ! $(grep "/userdata" $i | grep ":v2") = "" ]]; then
+				send_log "ROM FBE v2 support..."
+				finish
+			else
+				send_log "Change FBE v1 support..."
+				sed -i "s|fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized,keydirectory=/metadata/vold/metadata_encryption|fileencryption=ice,reservedsize=128M|" $fstabr
+				if [[ ! $(grep "/userdata" $i | grep wrappedkey) = "" ]]; then
+					send_log "...and enabled wrappedkey."
+					sed -i -e "s/formattable/formattable,wrappedkey/g" $fstabr
+				fi
+				finish
+			fi
+		fi
+	done
+}
+
+patch_fstab
+
+exit 0
