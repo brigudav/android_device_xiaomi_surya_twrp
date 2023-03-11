a750fd0d5e176d421f0522f24ee632937cbbecf1
Alexander <alexvl1972@hotmail.com>
2023-03-11 19:39:46 +1000

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,9 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-	android.hardware.fastboot@1.0-impl-mock \
-	android.hardware.fastboot@1.0-impl-mock.recovery \
-	fastbootd
+    android.hardware.fastboot@1.1-impl-mock
 
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
@@ -29,13 +27,19 @@ PRODUCT_SOONG_NAMESPACES += \
 # Dynamic partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Apex libraries
+PRODUCT_COPY_FILES += \
+    $(OUT_DIR)/target/product/$(PRODUCT_RELEASE_NAME)/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libcuuc.so
+
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
 	libion \
 	libxml2 \
 	vendor.display.config@1.0 \
 	vendor.display.config@2.0 \
-	libdisplayconfig.qti
+	libdisplayconfig.qti \
+	libandroidicu \
+	libdrm
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
 	$(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
