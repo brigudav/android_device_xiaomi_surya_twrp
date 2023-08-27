920a7d66de5306ccb6ed4ad5225a234c095b77be
Alexander <alexvl1972@hotmail.com>
2023-08-27 12:18:22 +1000

    surya: remove unneeded items
    - libxml2 and fastbootd is included upstream

--- device.mk
+++ device.mk
@@ -14,9 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-	android.hardware.fastboot@1.0-impl-mock \
-	android.hardware.fastboot@1.0-impl-mock.recovery \
-	fastbootd
+	android.hardware.fastboot@1.0-impl-mock
 
 # API
 PRODUCT_SHIPPING_API_LEVEL := 29
@@ -32,14 +30,12 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
 	libion \
-	libxml2 \
 	vendor.display.config@1.0 \
 	vendor.display.config@2.0 \
 	libdisplayconfig.qti
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
 	$(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-	$(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
 	$(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
 	$(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
 	$(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so
