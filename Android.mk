2d7c87c08c8b31f5b5dd5e265e42897b7896b777
Alexander <alexvl1972@hotmail.com>
2023-09-19 10:31:06 +1000

    remove irrelevant
    fixed https://github.com/TeamWin/android_bootable_recovery/commit/44e2346a60d34f12af1afcadfe0bb6dfbc79ee3a

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 #
 # Copyright 2018 The Android Open Source Project
-# Copyright 2014-2022 The Team Win LLC
+# Copyright 2014-2023 The Team Win LLC
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,7 +12,5 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 # Hack some props to allow stock ROM flashing
 BOARD_RECOVERY_IMAGE_PREPARE := \
-    sed -i 's/ro.product.board=.*/ro.product.board=surya/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
-    sed -i 's/ro.bootimage.build.date.utc=.*/ro.bootimage.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default; \
-    sed -i 's/ro.build.date.utc=.*/ro.build.date.utc=0/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default;
+    sed -i 's/ro.product.board=.*/ro.product.board=surya/' $(TARGET_RECOVERY_ROOT_OUT)/prop.default;
 endif
