4b156c6cfd10094271b58eff4f54529e88be8aef
Alexander <alexvl1972@hotmail.com>
2023-11-23 20:23:11 +1000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -149,6 +149,7 @@ TARGET_USES_MKE2FS := true
 TW_NO_SCREEN_BLANK := true
 TW_EXCLUDE_APEX := true
 TW_SUPPORT_INPUT_1_2_HAPTICS := true
+USE_QTI_HAPTICS := true
 
 # The path to a temperature sensor
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone19/temp"
