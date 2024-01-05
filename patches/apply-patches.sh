792f32e92181dd053cd380d7ec79e335b9dbbbc7
Alexander <alexvl1972@hotmail.com>
2024-01-06 01:11:32 +1000

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd ~
-cd ~/workspace/bootable/recovery
-cd ~/bootable/recovery
+CUR_DIR=$PWD
+cd ~/${GITHUB_WORKSPACE}/workspace/workspace/bootable/recovery
 patch -p1 < ../../device/xiaomi/surya/patches/0001-revert-save-settings-file-in-persist.patch
 cd ../..
