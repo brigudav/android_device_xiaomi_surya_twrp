cbb651aebfc80ca69b885c2b3434936e5ee3640c
Alexander <alexvl1972@hotmail.com>
2024-01-07 15:42:29 +1000

    Update apply-patches.sh

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd ./bootable/recovery
-patch -p1 < ../../device/Tecno/Camon18P/patches/0001-revert-save-settings-file-in-persist.patch
+patch -p1 < ../../device/xiaomi/surya/patches/0001-revert-save-settings-file-in-persist.patch
 cd ../..
